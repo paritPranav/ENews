64d9242034d8af61ab0641596292f0a149f6ad39
GauravChawda <grchawda16@gmail.com>
2022-10-29 15:34:46 +0530

    formidable added

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const express =require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose=require("mongoose");
-require('dotenv/config');
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-const app=express();
-    
-app.use(bodyParser.json())
+const app = express();
 
-mongoose.connect(process.env.DB_Connect, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-},()=>{
-    console.log("DB_Connection Successfull");
-});
+app.use(bodyParser.json());
 
+mongoose.connect(
+	process.env.DB_Connect,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	() => {
+		console.log("DB_Connection Successfull");
+	}
+);
 
 //import routes
-const PostRoute=require("./routes/posts");
-const UserRoute=require("./routes/user");
+const PostRoute = require("./routes/posts");
+const UserRoute = require("./routes/user");
 
 //middleware
-app.use("/posts",PostRoute);
-app.use("/user",UserRoute);
-
+app.use("/posts", PostRoute);
+app.use("/user", UserRoute);
 
 //Home get request
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+	// const newpost=new Posts({
+	//     Title:"Bank Blast at Dattchaowk",
+	//     newsContent:"two Robbers done unsuccesfull attempy to blast the bank"
+	// });
+	// newpost.save();
+	res.send("Home");
+	console.log("Done");
+});
 
-    // const newpost=new Posts({
-    //     Title:"Bank Blast at Dattchaowk",
-    //     newsContent:"two Robbers done unsuccesfull attempy to blast the bank"
-    // });
-    // newpost.save();
-    res.send("Home")
-    console.log("Done");
-})
-
- app.listen(3009,()=>{
-        console.log("running on port 3000");
- })
+app.listen(3000, () => {
+	console.log("running on port 3000");
+});
--- routes/user.js
+++ routes/user.js
@@ -1,85 +1,76 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const User=require('../models/users');
-const md5=require('md5');
-const jwt = require('jsonwebtoken');
-require('dotenv/config');
+const User = require("../models/users");
+const md5 = require("md5");
+const jwt = require("jsonwebtoken");
+const formidable = require("formidable");
 
-const path = require('path');
+require("dotenv/config");
+
+const path = require("path");
 
 //create and assign a token
 
-
-
-const app=express();
- 
+const app = express();
 
 app.use(bodyParser.json());
 const router = express.Router();
 
-
 //get all users
 
-router.get('/',async(req,res)=>{
-
-    try{
-        const users=await User.find();
-        res.json(users);
-    }catch(err){
-        res.send(err);
-    }
-})
-
-
-
-
-//New registreation ;
-router.post('/register',async(req,res)=>{
-
-    console.log("registreation..........");
-    console.log(req.body);
-    const idExist=await User.findOne({User_Id:req.body.id});
-    if(idExist)return res.status(400).send("is already exist")
-
-    const newuser= new User({
-        User_Id:req.body.id,
-        Password:md5(req.body.pass)
-    });
-
-   try{
-    const newRegister= await newuser.save();
-    res.json(newRegister);
-   }catch(err){
-        res.send(err);
-   }
-
-
+router.get("/", async (req, res) => {
+	try {
+		const users = await User.find();
+		res.json(users);
+	} catch (err) {
+		res.send(err);
+	}
 });
 
+//New registreation ;
+router.post("/register", async (req, res) => {
+	console.log("registreation..........");
+	console.log(req.body);
+	const idExist = await User.findOne({ User_Id: req.body.id });
+	if (idExist) return res.status(400).send("is already exist");
 
-router.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname, '../login.html'));
+	const newuser = new User({
+		User_Id: req.body.id,
+		Password: md5(req.body.pass),
+	});
 
-})
-//login 
-router.post('/login', async(req,res)=>{
+	try {
+		const newRegister = await newuser.save();
+		res.json(newRegister);
+	} catch (err) {
+		res.send(err);
+	}
+});
 
-    console.log(req.body);
-    console.log("Here 1");
-    const idExist=await User.findOne({User_Id:req.body.id, Password:md5(req.body.pass)});
-    console.log("here2");
-    if(!idExist)return res.status(400).send("Check password or UserId correctly");
+router.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, "../login.html"));
+});
+//login
+router.post("/login", async (req, res) => {
+	const form = formidable();
+	form.parse(req, async (err, fields, files) => {
+		if (err) {
+			return res.status(500).send("Internal server error");
+		}
+		console.log(fields);
+		const idExist = await User.findOne({
+			User_Id: fields.id,
+			Password: md5(fields.pass),
+		});
+		console.log("here2");
+		if (!idExist)
+			return res.status(400).send("Check password or UserId correctly");
 
-   
-    const token=jwt.sign({_id:User._id},process.env.TOKEN_SECRET);
-    res.header('auth-token',token).send(token);
-    //res.send("loged in");
-   
+		const token = jwt.sign({ _id: User._id }, process.env.TOKEN_SECRET);
+		res.header("auth-token", token).send(token);
+	});
 
+	//res.send("loged in");
+});
 
-
-})
-
-
-
-module.exports=router;
+module.exports = router;
